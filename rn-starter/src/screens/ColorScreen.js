5a5c7be412e5f7c7b65788b774765af472de9e7e
ViCabral <vinicius.cabral@infojrunesp.com.br>
2020-03-23 19:21:24 -0300

    Random Colors Included

--- rn-starter/src/screens/ColorScreen.js
+++ rn-starter/src/screens/ColorScreen.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React , {useState} from "react";
 import { Text, StyleSheet, View, Button } from "react-native";
 
 
 const ColorScreen = props => {
+
+  const [colors, setColors] = useState([]);
+
     return(
       <View>
       <Button 
       title="Add Color" 
-      //onPress = {() => navigation.navigate('Image')}
+      onPress = {() => setColors([...colors, randomRgb()])}
       />
+
+      <View 
+      style={{width: 100, height: 100, backgroundColor: randomRgb() }}
       
+      />
       </View>
     );
 };
+
+const randomRgb = () => {
+
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  return `rgb(${red}, ${green}, ${blue})`;
+}
 
 const styles = StyleSheet.create({
   text: {
